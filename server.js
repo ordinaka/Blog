42dcae1c3d8297e0d8989ed6b4f3f48653f13fc5
ordinaka <victorordinaka@gmail.com>
2025-10-01 13:17:11 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,78 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const port = 4000;
+const API_URL = "http://localhost:3000";
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Route to render the main page
+app.get("/", async (req, res) => {
+  try {
+    const response = await axios.get(`${API_URL}/posts`);
+    res.render("index.ejs", { posts: response.data });
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching posts" });
+  }
+});
+
+// Route to render the new post page
+app.get("/new", (req, res) => {
+  res.render("modify.ejs", { heading: "New Post", submit: "Create Post" });
+});
+
+// Route to render the edit page in the modify.ejs                                                       
+app.get("/edit/:id", async (req, res) => {
+  try {
+    const response = await axios.get(`${API_URL}/posts/${req.params.id}`);
+    console.log(response.data);
+    res.render("modify.ejs", {
+      heading: "Edit Post",
+      submit: "Update Post",
+      post: response.data,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching post" });
+  }
+});
+
+// Create a new post
+app.post("/api/posts", async (req, res) => {
+  try {
+    await axios.post(`${API_URL}/posts`, req.body);
+    res.redirect("/");
+  } catch (error) {
+    res.status(500).json({ message: "Error creating post" });
+  }
+});
+
+// Route to submit the edit post using patch
+app.post("/api/posts/:id", async (req, res) => {
+  try {
+     await axios.patch(
+      `${API_URL}/posts/${req.params.id}`,
+      req.body
+    );
+    res.redirect("/");
+  } catch (error) {
+    res.status(500).json({ message: "Error updating post" });
+  }
+});
+
+// Delete a post
+app.get("/api/posts/delete/:id", async (req, res) => {
+  try {
+    await axios.delete(`${API_URL}/posts/${req.params.id}`);
+    res.redirect("/");
+  } catch (error) {
+    res.status(500).json({ message: "Error deleting post" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Backend server is running on http://localhost:${port}`);
+});
